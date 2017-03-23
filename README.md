73ae55783e2b4404a0596a65000deb1c2b140641
Joaquín Fernández <jf@joaquinfernandez.net>
2017-03-23 20:51:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,116 @@
+# jfFileSystem [![stable](http://badges.github.io/stability-badges/dist/stable.svg)](http://github.com/badges/stability-badges)
+
+Class for manipulating file system in a synchronous way using `NodeJS`.
+
+You can create/remove directories recursively and read/write files too.
+
+I use it mainly as base for file generators.
+
+## Usage
+
+[![npm install jfFileSystem](https://nodei.co/npm/jf-file-system.png?compact=true)](https://npmjs.org/package/jf-file-system/)
+
+### Example
+
+#### Generator class
+
+```js
+const jfFileSystem = require('jf-file-system');
+const path         = require('path');
+const tr           = require('./translations');
+
+class Generator extends jfFileSystem {
+    constructor(directory)
+    {
+        this.directory = directory;
+        // Clean directory recursively before generating files.
+        this.rmdir(directory);
+    }
+    
+    generate(data)
+    {
+        // If outfile has several levels of depth, no problem.
+        // `write` method will create all required directories.
+        data.forEach(
+            config => this.write(
+                path.join(
+                    this.directory,
+                    config.outfile
+                ),
+                this.parse(config.data)
+            )
+        );
+    }
+    
+    log(level, label, ...args)
+    {
+        // Translating logs.
+        // tr is a map with translations.
+        super.log(level, tr[label], ...args);
+    }
+    
+    parse(data)
+    {
+        // In your class, process data and convert it to string.
+        return JSON.stringify(data);
+    }
+}
+```
+
+#### Translating texts
+
+All texts are in spanish but if you want to translate you can overwrite 
+`log` method in child class (as in the previous example) or to listen
+`log` event.
+
+```js
+const jfFileSystem = require('jf-file-system');
+const chalk        = require('chalk');
+const siNumber     = require('si-number');
+const fs           = new jfFileSystem();
+fs.on(
+    'log',
+    data =>
+    {
+        // Show only errors
+        if (data.level === 'error')
+        {
+            const _args = data.args;
+            if (Array.isArray(_args))
+            {
+                _args.forEach(
+                    (arg, index) =>
+                    {
+                        if (typeof arg === 'number')
+                        {
+                            // Format number in green using SI prefixes.
+                            _args[index] = chalk.green(
+                                siNumber(
+                                    arg,
+                                    {
+                                        decimal   : ',',
+                                        precision : 1,
+                                        thousands : true
+                                    }
+                                )
+                            );
+                        }
+                        else
+                        {
+                            // Texts in cyan.
+                            _args[index] = chalk.cyan(arg);
+                        }
+                    }
+                )
+            }
+        }
+        else
+        {
+            delete data.label;
+        }
+    }
+);
+fs.log('info', 'Test %s', 'pl1'); // Omitted because is not an error.
+fs.log('error', 'File %s already exists', '/tmp/exists.js'); // Filename in cyan
+fs.log('error', 'Filesize %s', 1324); // Number formatted in green as 1,3k
+```
---
+++ index.js
@@ -0,0 +1,206 @@
+const Events = require('events');
+const fs     = require('fs');
+const path   = require('path');
+/**
+ * Clase de utilidad para manejar el sistema de archivos de manera síncrona.
+ *
+ * @namespace jf
+ * @class     jf.FileSystem
+ * @extends   Events
+ */
+module.exports = class jfFileSystem extends Events {
+    /**
+     * Indica si una ruta existe.
+     *
+     * @method exists
+     *
+     * @param {String} pathTo Ruta a verificar.
+     *
+     * @return {Boolean} `true` si la ruta existe.
+     */
+    exists(pathTo)
+    {
+        return fs.existsSync(path.join(...arguments));
+    }
+
+    /**
+     * Muestra una traza por pantalla.
+     * Antes de mostrarla se dispara un evento y se pasa por referencia
+     * la etiqueta por si necesita traducirse.
+     *
+     * @method log
+     *
+     * @param {String}   level Nivel de la traza ('error', 'info', etc).
+     * @param {String}   label Texto a mostrar con los placeholders incluidos.
+     * @param {...Array} args  Argumentos a usar para reemplazar los placeholders de la etiqueta.
+     */
+    log(level, label, ...args)
+    {
+        const _payload = {
+            level,
+            label,
+            args
+        };
+        this.emit('log', _payload);
+        // Si algún manejador elimina la etiqueta, evitamos mostrar la traza por pantalla.
+        if (_payload.label)
+        {
+            console.log(_payload.label, ..._payload.args);
+        }
+    }
+
+    /**
+     * Crea un directorio de manera recursiva.
+     *
+     * @method mkdir
+     *
+     * @param {String} dir Ruta a crear.
+     */
+    mkdir(dir)
+    {
+        dir = path.join(...arguments);
+        if (!(this.exists(dir) && fs.statSync(dir).isDirectory()))
+        {
+            this.mkdir(path.dirname(dir));
+            fs.mkdirSync(dir);
+            this.log('info', 'Directorio %s creado', dir);
+        }
+    }
+
+    /**
+     * Devuelve el contenido de un archivo.
+     *
+     * @method read
+     *
+     * @param {String} filename Ruta del archivo leer.
+     *
+     * @return {String} Contenido del archivo.
+     */
+    read(filename)
+    {
+        return fs.readFileSync(path.join(...arguments), 'utf8');
+    }
+
+    /**
+     * Elimina el contenido del directorio de manera recursiva.
+     * Si se especifica un filtro, solamente se eliminan los archivos
+     * donde el filtro devuelva `true`.
+     * Se elimina un directorio si queda vacío.
+     * Si no se quiere eliminar el directorio, se debe usar el método `clean`.
+     *
+     * @method rmdir
+     *
+     * @param {String}   dir     Ruta al directorio del cual se eliminará TODO su contenido.
+     * @param {Number}   level   Número de niveles que mostrarán trazas.
+     * @param {Function} filter  Función a usar para filtrar los archivos que se eliminarán.
+     *
+     * @return {Number} Total de archivos eliminados.
+     */
+    rmdir(dir, level = 0, filter = null)
+    {
+        let _count = 0;
+        if (this.exists(dir) && fs.statSync(dir).isDirectory())
+        {
+            const _hasFilter = typeof filter === 'function';
+            if (level > 0)
+            {
+                this.log('info', 'Eliminando archivos en %s', path.relative(__dirname, dir));
+            }
+            fs.readdirSync(dir).forEach(
+                (file) =>
+                {
+                    const _filename = path.join(dir, file);
+                    const _stat     = fs.statSync(_filename);
+                    if (_stat.isDirectory())
+                    {
+                        _count += this.rmdir(_filename, level - 1, filter);
+                    }
+                    else if (_stat.isFile() && (!_hasFilter || filter(_filename)))
+                    {
+                        // Solamente lo eliminamos si pasa el filtro.
+                        fs.unlinkSync(_filename);
+                        ++_count;
+                    }
+                }
+            );
+            // Si el directorio está vacío, eliminamos el directorio.
+            if (this.scandir(dir).length === 0)
+            {
+                fs.rmdirSync(dir);
+            }
+            if (level > 0)
+            {
+                this.log('info', '%s archivo(s) eliminado(s)', _count);
+            }
+        }
+        return _count;
+    }
+
+    /**
+     * Devuelve el listado de archivos presentes en un directorio de manera recursiva.
+     *
+     * @method scandir
+     *
+     * @param {String}  dir    Directorio donde empezará la búsqueda.
+     * @param {RegExp?} filter Expresión regular para filtrar el resultado.
+     *
+     * @return {String[]} Listado de archivos.
+     */
+    scandir(dir, filter)
+    {
+        const _files = [];
+        if (this.exists(dir))
+        {
+            const _stats = fs.statSync(dir);
+            if (_stats.isDirectory())
+            {
+                fs.readdirSync(dir).forEach(
+                    (file) =>
+                    {
+                        const _filename = path.join(dir, file);
+                        const _stat     = fs.statSync(_filename);
+                        if (_stat.isDirectory())
+                        {
+                            _files.push.apply(_files, this.scandir(_filename, filter));
+                        }
+                        else if (_stat.isFile())
+                        {
+                            if (!filter || filter.test(_filename))
+                            {
+                                _files.push(_filename);
+                            }
+                        }
+                    }
+                );
+            }
+            else if (_stats.isFile())
+            {
+                _files.push(dir);
+            }
+            else
+            {
+                this.log('info', 'Tipo de archivo no soportado: %s', dir);
+            }
+        }
+        else
+        {
+            this.log('info', 'Directorio no encontrado: %s', dir);
+        }
+        return _files.sort();
+    }
+
+    /**
+     * Escribe el contenido en un archivo.
+     *
+     * @method write
+     *
+     * @param {String} filename Ruta del archivo escribir.
+     * @param {String} content  Contenido del archivo a crear.
+     */
+    write(filename, content)
+    {
+        this.mkdir(path.dirname(filename));
+        this.log('info', 'Escribiendo %s bytes en el archivo %s', content.length, filename);
+        fs.writeFileSync(filename, content, 'utf8');
+    }
+};
