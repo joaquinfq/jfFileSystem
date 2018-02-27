09966329d79fca08a0435b7ac3f4530eb7b2a13e
Joaquín Fernández <jf@joaquinfernandez.net>
2018-02-27 13:07:10 +0100

    Agregados los métodos `copy`, `isDirectory`, `isFile` y `rename`, mejorada la recursividad en `mkdir` y `rmdir` y los métodos `read` y `write` soportan `Buffer` además de textos.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path   = require('path');
  *
  * @type {null|jf.FileSystem}
  */
-let instance  = null;
+let instance = null;
 /**
  * Clase de utilidad para manejar el sistema de archivos de manera síncrona.
  *
@@ -14,7 +14,29 @@ let instance  = null;
  * @class     jf.FileSystem
  * @extends   Events
  */
-module.exports = class jfFileSystem extends Events {
+module.exports = class jfFileSystem extends Events
+{
+    /**
+     * Copia el contenido de un archivo o directorio a otra ubicación.
+     *
+     * @method copy
+     *
+     * @param {String} fromDir Directorio origen.
+     * @param {String} toDir   Directorio destino.
+     */
+    copy(fromDir, toDir)
+    {
+        this.scandir(fromDir).forEach(
+            file => this.write(
+                file === fromDir
+                    ? path.join(toDir, path.basename(file))
+                    : path.join(toDir, path.relative(fromDir, file)),
+                this.read(file, null),
+                null
+            )
+        );
+    }
+
     /**
      * Indica si una ruta existe.
      *
@@ -30,9 +52,45 @@ module.exports = class jfFileSystem extends Events {
     }
 
     /**
+     * Indica si la ruta especificada es un directorio.
+     *
+     * @method isDirectory
+     *
+     * @param {String} path Ruta a verificar.
+     *
+     * @return {Boolean} `true` si la ruta existe y es un directorio.
+     */
+    isDirectory(path)
+    {
+        return this.exists(path)
+            ? fs.statSync(path).isDirectory()
+            : false;
+    }
+
+    /**
+     * Indica si la ruta especificada es un archivo.
+     *
+     * @method isFile
+     *
+     * @param {String} path Ruta a verificar.
+     *
+     * @return {Boolean} `true` si la ruta existe y es un archivo.
+     */
+    isFile(path)
+    {
+        return this.exists(path)
+            ? fs.statSync(path).isFile()
+            : false;
+    }
+
+    /**
      * Muestra una traza por pantalla.
+     *
      * Antes de mostrarla se dispara un evento y se pasa por referencia
-     * la etiqueta por si necesita traducirse.
+     * la etiqueta por si necesita traducirse o hacer algún cambio.
+     *
+     * Si el manejador elimina el atributo `label`, no se muestra la
+     * traza por pantalla.
      *
      * @method log
      *
@@ -47,17 +105,17 @@ module.exports = class jfFileSystem extends Events {
         {
             name = this.constructor.name;
         }
-        const _payload = {
-            level,
-            label,
-            name,
-            args
-        };
-        this.emit('log', _payload);
-        // Si algún manejador elimina la etiqueta, evitamos mostrar la traza por pantalla.
-        if (_payload.label)
+        //------------------------------------------------------------------------------
+        // Los datos se pasan en un objeto para que puedan ser modificados.
+        // Si algún manejador del evento `log` elimina la etiqueta,
+        // no se mostrará la traza por pantalla.
+        // Este manejador podrá traducir la etiqueta si es necesario.
+        //------------------------------------------------------------------------------
+        const _data = { level, label, name, args };
+        this.emit('log', _data);
+        if (_data.label)
         {
-            console.log(`[${name}] ${_payload.label}`, ..._payload.args);
+            console[_data.level](`[${_data.name}] ${_data.label}`, ..._data.args);
         }
     }
 
@@ -71,7 +129,7 @@ module.exports = class jfFileSystem extends Events {
     mkdir(dir)
     {
         dir = path.join(...arguments);
-        if (!(this.exists(dir) && fs.statSync(dir).isDirectory()))
+        if (!this.isDirectory(dir))
         {
             this.mkdir(path.dirname(dir));
             fs.mkdirSync(dir);
@@ -85,66 +143,79 @@ module.exports = class jfFileSystem extends Events {
      * @method read
      *
      * @param {String} filename Ruta del archivo leer.
+     * @param {String} encoding Codificación del archivo a leer.
+     *
+     * @return {String|Buffer} Contenido del archivo.
+     */
+    read(filename, encoding = 'utf8')
+    {
+        return fs.readFileSync(filename, encoding);
+    }
+
+    /**
+     * Renombra o mueve de ubicación un archivo.
+     *
+     * @method rename
+     *
+     * @param {String} oldPath Ruta del archivo a renombrar.
+     * @param {String} newPath Nueva ruta del archivo.
      *
      * @return {String} Contenido del archivo.
      */
-    read(filename)
+    rename(oldPath, newPath)
     {
-        return fs.readFileSync(path.join(...arguments), 'utf8');
+        return fs.renameSync(oldPath, newPath);
     }
 
     /**
      * Elimina el contenido del directorio de manera recursiva.
+     *
      * Si se especifica un filtro, solamente se eliminan los archivos
      * donde el filtro devuelva `true`.
+     *
      * Se elimina un directorio si queda vacío.
      * Si no se quiere eliminar el directorio, se debe usar el método `clean`.
      *
      * @method rmdir
      *
-     * @param {String}   dir     Ruta al directorio del cual se eliminará TODO su contenido.
-     * @param {Number}   level   Número de niveles que mostrarán trazas.
-     * @param {Function} filter  Función a usar para filtrar los archivos que se eliminarán.
+     * @param {String}  dir    Ruta al directorio del cual se eliminará TODO su contenido.
+     * @param {Number?} level  Número de niveles que mostrarán trazas.
+     * @param {RegExp?} filter Expresión regular a usar para filtrar los archivos que se eliminarán.
      *
      * @return {Number} Total de archivos eliminados.
      */
     rmdir(dir, level = 0, filter = null)
     {
         let _count = 0;
-        if (this.exists(dir) && fs.statSync(dir).isDirectory())
+        if (!filter || filter.test(dir))
         {
-            const _hasFilter = typeof filter === 'function';
-            if (level > 0)
+            const _stat = fs.lstatSync(dir);
+            if (_stat.isDirectory())
             {
-                this.log('info', '', 'Eliminando archivos en %s', path.relative(__dirname, dir));
-            }
-            fs.readdirSync(dir).forEach(
-                (file) =>
+                if (level > 0)
                 {
-                    const _filename = path.join(dir, file);
-                    const _stat     = fs.statSync(_filename);
-                    if (_stat.isDirectory())
-                    {
-                        _count += this.rmdir(_filename, level - 1, filter);
-                    }
-                    else if (_stat.isFile() && (!_hasFilter || filter(_filename)))
-                    {
-                        // Solamente lo eliminamos si pasa el filtro.
-                        fs.unlinkSync(_filename);
-                        ++_count;
-                    }
+                    this.log('info', '', 'Eliminando archivos en %s', path.relative(__dirname, dir));
                 }
-            );
-            // Si el directorio está vacío, eliminamos el directorio.
-            if (this.scandir(dir).length === 0)
+                fs.readdirSync(dir).forEach(
+                    file => _count += this.rmdir(path.join(dir, file), level - 1, filter)
+                );
+                // Si el directorio está vacío, eliminamos el directorio.
+                if (this.scandir(dir).length === 0)
+                {
+                    fs.rmdirSync(dir);
+                }
+            }
+            else if (_stat.isFile() || _stat.isSymbolicLink())
             {
-                fs.rmdirSync(dir);
+                fs.unlinkSync(dir);
+                ++_count;
             }
             if (level > 0)
             {
                 this.log('info', '', '%s archivo(s) eliminado(s)', _count);
             }
         }
+
         return _count;
     }
 
@@ -158,47 +229,37 @@ module.exports = class jfFileSystem extends Events {
      *
      * @return {String[]} Listado de archivos.
      */
-    scandir(dir, filter)
+    scandir(dir, filter = null)
     {
         const _files = [];
-        if (this.exists(dir))
+        if (!filter || filter.test(dir))
         {
-            const _stats = fs.statSync(dir);
-            if (_stats.isDirectory())
+            if (this.exists(dir))
             {
-                fs.readdirSync(dir).forEach(
-                    (file) =>
-                    {
-                        const _filename = path.join(dir, file);
-                        const _stat     = fs.statSync(_filename);
-                        if (_stat.isDirectory())
-                        {
-                            _files.push.apply(_files, this.scandir(_filename, filter));
-                        }
-                        else if (_stat.isFile())
-                        {
-                            if (!filter || filter.test(_filename))
-                            {
-                                _files.push(_filename);
-                            }
-                        }
-                    }
-                );
-            }
-            else if (_stats.isFile())
-            {
-                _files.push(dir);
+                const _stats = fs.lstatSync(dir);
+                if (_stats.isDirectory())
+                {
+                    fs.readdirSync(dir).forEach(
+                        file => _files.push(...this.scandir(path.join(dir, file), filter))
+                    );
+                }
+                else if (_stats.isFile())
+                {
+                    _files.push(dir);
+                }
+                else if (!_stats.isSymbolicLink())
+                {
+                    this.log('info', '', 'Tipo de archivo no soportado: %s', dir);
+                }
             }
             else
             {
-                this.log('info', '', 'Tipo de archivo no soportado: %s', dir);
+                this.log('warn', '', 'Directorio no encontrado: %s', dir);
             }
+            _files.sort();
         }
-        else
-        {
-            this.log('warn', '', 'Directorio no encontrado: %s', dir);
-        }
-        return _files.sort();
+
+        return _files;
     }
 
     /**
@@ -206,14 +267,15 @@ module.exports = class jfFileSystem extends Events {
      *
      * @method write
      *
-     * @param {String} filename Ruta del archivo escribir.
-     * @param {String} content  Contenido del archivo a crear.
+     * @param {String}        filename Ruta del archivo escribir.
+     * @param {Buffer|String} content  Contenido del archivo a crear.
+     * @param {String}        encoding Codificación del archivo a leer.
      */
-    write(filename, content)
+    write(filename, content, encoding = 'utf8')
     {
         this.mkdir(path.dirname(filename));
         this.log('info', '', 'Escribiendo %s bytes en el archivo %s', content.length, filename);
-        fs.writeFileSync(filename, content, 'utf8');
+        fs.writeFileSync(filename, content, encoding);
     }
 
     /**
